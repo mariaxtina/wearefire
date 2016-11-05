b3d550f5f6d16c64d450e8db8697a6ef3dc6adab
mariaxtina <mchristinahess@gmail.com>
2016-11-05 14:33:33 -0700

    Fix button styling to be a circle

--- components/Button.js
+++ components/Button.js
@@ -11,10 +11,10 @@ export default class Button extends React.Component {
   render() {
     return (
       <Components.LinearGradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={{padding: 15, alignItems: 'center', borderRadius: 5}}>
-        <Text style={{backgroundColor: 'transparent', fontSize: 15, color: '#fff'}}>
-          Set me on fire!
+        colors={['#FFDC01', '#FDAC01', '#F73B01']}
+        style={{alignItems: 'center', justifyContent: 'center', width: 200, height: 200, borderRadius: 100}}>
+        <Text style={{backgroundColor: 'transparent', fontSize: 30, color: '#fff'}}>
+          FIRE!
         </Text>
       </Components.LinearGradient>
     );
