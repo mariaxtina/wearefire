1eb019a0e681bbcd471ec1b8c175d6dd3c41621e
mariaxtina <mchristinahess@gmail.com>
2016-11-05 13:48:33 -0700

    Add button component

---
+++ components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+} from 'react-native';
+import {
+  Components
+} from 'exponent';
+
+export default class FacebookButton extends React.Component {
+  render() {
+    return (
+      <Components.LinearGradient
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={{padding: 15, alignItems: 'center', borderRadius: 5}}>
+        <Text style={{backgroundColor: 'transparent', fontSize: 15, color: '#fff'}}>
+          Set me on fire!
+        </Text>
+      </Components.LinearGradient>
+    );
+  }
+}
