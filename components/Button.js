e2616846195a73f7f7f1bf8f55682282a889e31c
mariaxtina <mchristinahess@gmail.com>
2016-11-05 14:48:15 -0700

    Add touchable highlight to handle keypress

--- components/Button.js
+++ components/Button.js
@@ -10,13 +10,25 @@ import {
 export default class Button extends React.Component {
   render() {
     return (
-      <Components.LinearGradient
-        colors={['#FFDC01', '#FDAC01', '#F73B01']}
-        style={{alignItems: 'center', justifyContent: 'center', width: 200, height: 200, borderRadius: 100}}>
-        <Text style={{backgroundColor: 'transparent', fontSize: 30, color: '#fff'}}>
-          FIRE!
-        </Text>
-      </Components.LinearGradient>
+      <TouchableOpacity onPress={this._handleHelpPress}>
+        <Components.LinearGradient
+          colors={['#FFDC01', '#FDAC01', '#F73B01']}
+          style={{alignItems: 'center', justifyContent: 'center', width: 200, height: 200, borderRadius: 100}}>
+          <Text style={{backgroundColor: 'transparent', fontSize: 30, color: '#fff'}}>
+            FIRE!
+          </Text>
+        </Components.LinearGradient>
+      </TouchableOpacity>
     );
+  }
+
+  _handleHelpPress = async () => {
+    try {
+      let result = await fetch('http://google.com');
+      let data = await result.text();
+      alert(JSON.stringify(result));
+    } catch(e) {
+      alert(e.message);
+    }
   }
 }
